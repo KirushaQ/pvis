ac9c4a9c14505c9b64e38e04d14c0395d51ec6e9
KirushaQ <33037184+KirushaQ@users.noreply.github.com>
2019-02-25 20:26:52 +0300

    Add files via upload
    2-ой пункт added

--- ppvis_1/src/ppvis_1/All.java
+++ ppvis_1/src/ppvis_1/All.java
@@ -10,6 +10,7 @@ import javax.swing.event.ChangeListener;*/
 public class All extends JFrame
 {
 	private JTextField text1;
+	JButton button1, button2;
     public All()
     {
     	start();
@@ -24,28 +25,34 @@ public class All extends JFrame
     	text1.setText("Input text");
     	this.add(text1);
     	
-        JButton button1=new JButton();
+        button1=new JButton();
         button1.setText("Button 1");
         button1.setBounds(300,30,250,35);
-        button1.addActionListener(new ListenerAction());
+        button1.addActionListener(new ListenerAction1());
        // button1.addChangeListener(new ListenerChange());
     	this.add(button1);
         
-        JButton button2=new JButton();
+        button2=new JButton();
         button2.setText("Button 2");
         button2.setBounds(300,80,250,35);
+        button2.addActionListener(new ListenerAction2());
     	this.add(button2);
     	
     }
     
-    class ListenerAction implements ActionListener {
+    class ListenerAction1 implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-        	String a;
-        	text1.getText();
-            System.out.println("Нажатие кнопки! От - "+ 
-                                e.getActionCommand() + "\n");
+        	button1.setText(text1.getText());
         }
-    }//при нажатии на конпку 
+    }//при нажатии на кнопку
+    
+    class ListenerAction2 implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        	String s=button1.getText();
+        	button1.setText(button2.getText());
+        	button2.setText(s);
+        }
+    }
     
   /*  class ListenerChange implements ChangeListener {
         public void stateChanged(ChangeEvent e) {
