c7a677531f321ee14595eb437c8d4c0792231f73
KirushaQ <33037184+KirushaQ@users.noreply.github.com>
2019-05-30 20:52:18 +0300

    fix baggs

--- ppvis_2/src/ppvis_2/Data.java
+++ ppvis_2/src/ppvis_2/Data.java
@@ -16,8 +16,27 @@ public class Data {
     ArrayList <String> winnerFName = new ArrayList<>();
     ArrayList <Integer> tPrise = new ArrayList<>();
     ArrayList <Integer> wPrise = new ArrayList<>();
+    
+	
     Data()
     {
-    	tName.add("ИЗ");
+ 
+    	tName.add("Боксыч");
+    	tDate.add(LocalDate.of(2014, 10, 10));
+    	sName.add("Бокс");
+    	winnerName.add("Вася");
+        winnerSName.add("Васильев");
+    	winnerFName.add("Васильевич");
+    	tPrise.add(1000);
+    	wPrise.add(tPrise.get(0)/10*6);
+    	
+    	tName.add("Тенисыч");
+    	tDate.add(LocalDate.of(2012, 12, 13));
+    	sName.add("Тенис");
+    	winnerName.add("Иван");
+    	winnerSName.add("Иванов");
+    	winnerFName.add("Иванович");
+    	tPrise.add(800);
+    	wPrise.add(tPrise.get(0)/10*6);
     }
 }
